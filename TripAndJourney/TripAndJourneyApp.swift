81cd67ad348b092537f95c4dfb9fbeb2b2dfd475
vmarch <marchenkoukr@gmail.com>
2021-09-12 14:23:23 +0200

    Initial Commit

---
+++ TripAndJourney/TripAndJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripAndJourneyApp.swift
+//  TripAndJourney
+//
+//  Created by Student on 12.09.21.
+//
+
+import SwiftUI
+
+@main
+struct TripAndJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
